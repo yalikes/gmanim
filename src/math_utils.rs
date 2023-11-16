9929d232c0348657fd91c97e2daacaf7ebfea3c6
yalikes <algebnaly@qq.com>
2023-11-16 23:36:53 +0800

    add test bezier curve function

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -1,8 +1,9 @@
 use std::ops::AddAssign;
 
 use nalgebra::Vector2;
+use raqote::{PathBuilder, SolidSource, StrokeStyle};
 
-use crate::GMFloat;
+use crate::{mobjects::{coordinate_change_x, coordinate_change_y}, Context, ContextType, GMFloat};
 
 pub fn bezier_curve(point_list: &[Vector2<GMFloat>], t: GMFloat) -> Vector2<GMFloat> {
     if point_list.len() < 2 {
@@ -86,4 +87,54 @@ pub fn lerp(
     t: GMFloat,
 ) -> nalgebra::Vector2<GMFloat> {
     return p0 * (1.0 - t) + p1 * t;
+}
+
+#[test]
+fn test_bezier_curve() {
+    let p0 = Vector2::new(0.0, 0.0);
+    let p1 = Vector2::new(0.5, 1.0);
+    let p2 = Vector2::new(1.0, 0.0);
+    let mut ctx = Context::default();
+    if let ContextType::Raqote(dt) = &mut ctx.ctx_type {
+        dt.clear(SolidSource {
+            r: 0x00,
+            g: 0x00,
+            b: 0x00,
+            a: 0xff,
+        });
+        let mut pb = PathBuilder::new();
+        pb.move_to(
+            coordinate_change_x(p0.x, ctx.scene_config.width) * ctx.scene_config.scale_factor,
+            coordinate_change_y(p0.y, ctx.scene_config.height) * ctx.scene_config.scale_factor,
+        );
+        let mut t = 0.0;
+        let delta_t = 1.0 / 20.0;
+        while t < 1.0 {
+            t += delta_t;
+            let p = bezier_cubic(p0, p1, p2, t);
+            println!("{:?}", p);
+            pb.line_to(
+                coordinate_change_x(p.x, ctx.scene_config.width) * ctx.scene_config.scale_factor,
+                coordinate_change_y(p.y, ctx.scene_config.height) * ctx.scene_config.scale_factor,
+            );
+        }
+        let path = pb.finish();
+        dt.stroke(
+            &path,
+            &raqote::Source::Solid(SolidSource {
+                r: 0xff,
+                g: 0xff,
+                b: 0,
+                a: 0xff,
+            }),
+            &&raqote::StrokeStyle {
+                cap: raqote::LineCap::Round,
+                join: raqote::LineJoin::Round,
+                width: (0.02 * ctx.scene_config.scale_factor) as f32,
+                ..Default::default()
+            },
+            &raqote::DrawOptions::new(),
+        );
+        dt.write_png("test_bezier_curve.png");
+    }
 }
